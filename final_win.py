9f34679523eafd8969fddae483afb5a1d8ab6dac
fff-12 <paunvprrv@gmail.com>
2024-05-04 18:32:29 +0300

    a

--- final_win.py
+++ final_win.py
@@ -27,69 +27,77 @@ class FinalWin(QWidget):
  
     def results(self):
         pass
-        # if self.exp.age < 7:
-        #     self.index = 0
-        #     return "немає даних для такого віку"
-        # self.index = (4 * (int(self.exp.t1) + int(self.exp.t2) + int(self.exp.t3)) - 200) / 10
-        # if self.exp.age == 7 or self.exp.age == 8:
-        #    if self.index >= 21:
-        #        return txt_res1
-        #    elif self.index < 21 and self.index >= 17:
-        #        return txt_res2
-        #    elif self.index < 17 and self.index >= 12:
-        #        return txt_res3
-        #    elif self.index < 12 and self.index >= 6.5:
-        #        return txt_res4
-        #    else:
-        #        return txt_res5
-        # if self.exp.age == 9 or self.exp.age == 10:
-        #    if self.index >= 19.5:
-        #        return txt_res1
-        #    elif self.index < 19.5 and self.index >= 15.5:
-        #        return txt_res2
-        #    elif self.index < 15.5 and self.index >= 10.5:
-        #        return txt_res3
-        #    elif self.index < 10.5 and self.index >= 5:
-        #        return txt_res4
-        #    else:
-        #        return txt_res5
-        # if self.exp.age == 11 or self.exp.age == 12:
-        #    if self.index >= 18:
-        #        return txt_res1
-        #    elif self.index < 18 and self.index >= 14:
-        #        return txt_res2
-        #    elif self.index < 14 and self.index >= 9:
-        #        return txt_res3
-        #    elif self.index < 9 and self.index >= 3.5:
-        #        return txt_res4
-        #    else:
-        #        return txt_res5
-        # if self.exp.age == 13 or self.exp.age == 14:
-        #    if self.index >= 16.5:
-        #        return txt_res1
-        #    elif self.index < 16.5 and self.index >= 12.5:
-        #        return txt_res2
-        #    elif self.index < 12.5 and self.index >= 7.5:
-        #        return txt_res3
-        #    elif self.index < 7.5 and self.index >= 2:
-        #        return txt_res4
-        #    else:
-        #        return txt_res5
-        # if self.exp.age >= 15:
-        #    if self.index >= 15:
-        #        return txt_res1
-        #    elif self.index < 15 and self.index >= 11:
-        #        return txt_res2
-        #    elif self.index < 11 and self.index >= 6:
-        #        return txt_res3
-        #    elif self.index < 6 and self.index >= 0.5:
-        #        return txt_res4
-        #    else:
-        #        return txt_res5
+        if self.exp.age < 7:
+            self.index = 0
+            return "немає даних для такого віку"
+        self.index = (4 * (int(self.exp.t1) + int(self.exp.t2) + int(self.exp.t3)) - 200) / 10
+        if self.exp.age == 7 or self.exp.age == 8:
+           if self.index >= 21:
+               return txt_res1
+           elif self.index < 21 and self.index >= 17:
+               return txt_res2
+           elif self.index < 17 and self.index >= 12:
+               return txt_res3
+           elif self.index < 12 and self.index >= 6.5:
+               return txt_res4
+           else:
+               return txt_res5
+        if self.exp.age == 9 or self.exp.age == 10:
+           if self.index >= 19.5:
+               return txt_res1
+           elif self.index < 19.5 and self.index >= 15.5:
+               return txt_res2
+           elif self.index < 15.5 and self.index >= 10.5:
+               return txt_res3
+           elif self.index < 10.5 and self.index >= 5:
+               return txt_res4
+           else:
+               return txt_res5
+        if self.exp.age == 11 or self.exp.age == 12:
+           if self.index >= 18:
+               return txt_res1
+           elif self.index < 18 and self.index >= 14:
+               return txt_res2
+           elif self.index < 14 and self.index >= 9:
+               return txt_res3
+           elif self.index < 9 and self.index >= 3.5:
+               return txt_res4
+           else:
+               return txt_res5
+        if self.exp.age == 13 or self.exp.age == 14:
+           if self.index >= 16.5:
+               return txt_res1
+           elif self.index < 16.5 and self.index >= 12.5:
+               return txt_res2
+           elif self.index < 12.5 and self.index >= 7.5:
+               return txt_res3
+           elif self.index < 7.5 and self.index >= 2:
+               return txt_res4
+           else:
+               return txt_res5
+        if self.exp.age >= 15:
+           if self.index >= 15:
+               return txt_res1
+           elif self.index < 15 and self.index >= 11:
+               return txt_res2
+           elif self.index < 11 and self.index >= 6:
+               return txt_res3
+           elif self.index < 6 and self.index >= 0.5:
+               return txt_res4
+           else:
+               return txt_res5
     ''' створює графічні елементи '''
     def initUI(self):  
-        pass
+        self.work_text = QLabel(txt_workheart + self.results())
+        self.index_text = QLabel(txt_index + str(self.index))
+ 
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
+        self.layout_line.addWidget(self.work_text, alignment = Qt.AlignCenter)
+        self.setLayout(self.layout_line)
  
     ''' встановлює, як виглядатиме вікно (напис, розмір, місце) '''
     def set_appear(self):
-        pass
+        self.setWindowTitle(txt_finalwin)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
